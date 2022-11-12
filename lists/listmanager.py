467e9e8f780a605447d7ff754595bef97dd8f45e
unknown <programmer08054@gmail.com>
2022-11-11 19:46:09 -0500

    got delete working

--- lists/listmanager.py
+++ lists/listmanager.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from os.path import isfile
+import datetime
 
 def get_list_path():
     # read list from file
@@ -10,13 +11,12 @@ def get_list():
     output = {}
     list_path = get_list_path()
     if not isfile(list_path):
-        list_file = open(list_path, 'w')
-        list_file.write('Example List,Item 1' + '\n')
-        list_file.write('Example List,Item 2' + '\n')
-        list_file.close()
+        write_list({'Example':['Item 1', 'Item 2']})
     list_file = open(list_path, 'r')
     Lines = list_file.readlines()
     for line in Lines:
+        if(line.startswith('//')):
+            continue
         parts = line.split(',', 2)
         if 2 > len(parts):
             continue
@@ -26,3 +26,13 @@ def get_list():
             output[key] = []
         output[key].append(value.strip())
     return output
+
+def write_list(list_content):
+    current_time = datetime.datetime.now()
+    list_path = get_list_path()
+    list_file = open(list_path, 'w')
+    list_file.write('// This file was created on ' + current_time.strftime('%b/%d/%Y') + '\n')
+    for group, items in list_content.items():
+        for item in items:
+            list_file.write(group + ',' + item + '\n')
+    list_file.close()
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .listmanager import get_list
+from .listmanager import get_list, write_list
 
 # Create your views here.
 def index(request):
@@ -9,3 +9,12 @@ def index(request):
     # print(list_path)
     list_content = get_list() 
     return render(request, 'list-template.html', context={'list': list_content})
+
+def delete(request):
+    group = request.GET.get('group', '')
+    name = request.GET.get('name', '')
+    list_content = get_list()
+    if group in list_content:
+        list_content[group].remove(name)
+    write_list(list_content)
+    return HttpResponse('Succesfully deleted')
