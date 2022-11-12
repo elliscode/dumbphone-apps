fbee3266279e6c0d002e8a2d2848b0dae5bbe18c
unknown <programmer08054@gmail.com>
2022-11-11 20:09:24 -0500

    list ordering working now

--- lists/urls.py
+++ lists/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete', views.delete, name='delete'),
+    path('add', views.add, name='add'),
+    path('move', views.move, name='move'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -18,3 +18,39 @@ def delete(request):
         list_content[group].remove(name)
     write_list(list_content)
     return HttpResponse('Succesfully deleted')
+
+def add(request):
+    group = request.GET.get('group', 'Groceries')
+    name = request.GET.get('name', '')
+    list_content = get_list()
+    if group not in list_content:
+        list_content[group] = []
+    list_content[group].append(name)
+    write_list(list_content)
+    return HttpResponse('Succesfully added')
+
+def move(request):
+    direction = request.GET.get('direction', 'up')
+    group = request.GET.get('group', '')
+    list_content = get_list()
+    groups = list(list_content.keys())
+    if group not in groups:
+        return HttpResponse('Group does not exist')
+    index = groups.index(group)
+    if 'up' == direction:
+        if 0 == index:
+            return HttpResponse('Group cannot go any higher')
+        temp = groups[index - 1]
+        groups[index - 1] = group
+        groups[index] = temp
+    else:
+        if len(groups) - 1 == index:
+            return HttpResponse('Group cannot go any lower')
+        temp = groups[index + 1]
+        groups[index + 1] = group
+        groups[index] = temp
+    reordered_list = {}
+    for group in groups:
+        reordered_list[group] = list_content[group]
+    write_list(reordered_list)
+    return HttpResponse('Succesfully moved')
