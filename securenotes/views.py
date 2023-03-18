37ba4fe9553955c12e015ea67a4857b950823d05
elliscode <45629640+elliscode@users.noreply.github.com>
2023-03-17 20:53:39 -0400

    working encrypted notes app

--- securenotes/views.py
+++ securenotes/views.py
@@ -2,8 +2,9 @@ import datetime
 import uuid
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.db.models import QuerySet
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 from django.shortcuts import render
 
 from dumbphoneapps.settings import LOGIN_URL
@@ -13,21 +14,24 @@ from securenotes.models import SecureNote
 # Create your views here.
 @login_required(login_url=LOGIN_URL)
 def index(request):
-    return render(request, 'securenotes/index.html')
-
-
-@login_required(login_url=LOGIN_URL)
-def get_encrypted_data(request):
     samples_list: list[SecureNote] = SecureNote.objects.filter(user=request.user, )
     output_list = []
     for sample in samples_list:
         output_list.append({
             'hash': sample.hash,
             'timestamp': sample.time_stamp,
-            'encrypted_text': sample.encrypted_text,
             'encrypted_sample': sample.encrypted_sample,
         })
-    return JsonResponse({'encrypted_samples': output_list})
+    return render(request, 'securenotes/index.html', context={'notes': output_list})
+
+
+@login_required(login_url=LOGIN_URL)
+def get_encrypted_data(request: HttpRequest):
+    hash_value = request.GET.get('hash', None)
+    note: SecureNote = SecureNote.objects.filter(user=request.user, hash=hash_value).first()
+    if note is not None:
+        return JsonResponse({'status': 'success', 'note': note.encrypted_text})
+    return JsonResponse({'status': 'fail', 'message': 'No note found with hash value'})
 
 
 @login_required(login_url=LOGIN_URL)
@@ -37,13 +41,14 @@ def post_encrypted_data(request):
     hash_value = request.POST.get('hash', uuid.uuid4())
     note: SecureNote = SecureNote.objects.filter(user=request.user, hash=hash_value, ).first()
     if note:
-        # check if the encrypted sample you are sending matches the encrypted sample we have
-        if encrypted_sample != note.encrypted_sample:
-            return JsonResponse({'status': 'fail', 'message': 'Encrypted sample string provided did not match'})
+        note.encrypted_sample = encrypted_sample
         note.encrypted_text = encrypted_note
-        note.time_stamp = datetime.datetime.now()
     else:
-        note = SecureNote(user=request.user, hash=hash_value, encrypted_text=encrypted_note, encrypted_sample=encrypted_sample)
+        note = SecureNote(user=request.user, hash=hash_value, encrypted_text=encrypted_note,
+                          encrypted_sample=encrypted_sample)
     note.save()
-    return JsonResponse({'status': 'success', 'message': 'Successfully saved record', 'hash': note.hash})
-
+    return JsonResponse({'status': 'success',
+                         'message': 'Successfully saved record',
+                         'hash': note.hash,
+                         'timestamp': note.time_stamp,
+                         'encrypted_sample': note.encrypted_sample, })
