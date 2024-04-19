65efa003b3407c8616c5ae2030a30c57422630cf
elliscode <45629640+elliscode@users.noreply.github.com>
2024-04-18 20:41:11 -0400

    chmod

--- release.sh
+++ release.sh
@@ -25,8 +25,8 @@ if $s3; then
 fi
 if ! $lambda && ! $s3; then
     echo "You can supply either l or s, or ls, you supplied nothing, so I will do nothing";
-    echo "e.g.";
-    echo "> sh ${ENV}-release.sh ls";
+    echo "";
+    echo "./${ENV}-release.sh ls";
 fi
 
 if $lambda; then
