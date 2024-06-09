a67ed6b65fca909a022b97d409bb8ffa1a20b41d
elliscode <45629640+elliscode@users.noreply.github.com>
2024-06-09 18:54:40 -0400

    cache bust

--- s3/grocery-list/js/list.js
+++ s3/grocery-list/js/list.js
@@ -409,10 +409,10 @@ if (
   navigator.userAgent.includes("Safari")
 ) {
   iosCookieRefresh();
-  setStylesheet("css/grocery-list-new.css?v=014");
+  setStylesheet("css/grocery-list-new.css?v=015");
   document.getElementById("item-text-box").addEventListener("blur", startHide);
 } else {
-  setStylesheet("css/grocery-list-old.css?v=014");
+  setStylesheet("css/grocery-list-old.css?v=015");
 }
 const loader = document.getElementById("loading");
 const dontDisplayWhileLoading = document.getElementById(
